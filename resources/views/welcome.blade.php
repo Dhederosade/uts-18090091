b473f17f9c17a0f034e0eed19c96168d5edda2c1
febianissa <febiannisa22@gmail.com>
2021-04-22 16:25:30 +0700

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,7 +48,7 @@
 
     <!-- footer section start -->
     <footer>
-        <span>Created By <a href="#">Getar Jagat Dewantara</a> | <span class="far fa-copyright"></span> 2020 All rights reserved.</span>
+        <span>Created By <a href="#">Revan Febriansyah</a> | <span class="far fa-copyright"></span> 2020 All rights reserved.</span>
     </footer>
 
     <script src="script.js"></script>
